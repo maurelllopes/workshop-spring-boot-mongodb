eff07b184e60c8318ada77971115714b5fdf2dbc
Alexandre Maurell <maurelllopes@mac.com>
2021-05-14 03:08:42 -0300

    Obtendo um usuario por id

--- src/main/java/com/maurelllopes/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/maurelllopes/workshopmongo/repository/UserRepository.java
@@ -7,5 +7,7 @@ import com.maurelllopes.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+	
 	
 }
--- src/main/java/com/maurelllopes/workshopmongo/services/UserService.java
+++ src/main/java/com/maurelllopes/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.maurelllopes.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.maurelllopes.workshopmongo.domain.User;
 import com.maurelllopes.workshopmongo.repository.UserRepository;
+import com.maurelllopes.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,6 +20,11 @@ public class UserService {
 		
 		return repo.findAll();
 	}
-	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+		
+	}
 
-}
+
