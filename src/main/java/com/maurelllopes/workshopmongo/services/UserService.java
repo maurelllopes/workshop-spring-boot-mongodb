def37cef1475d36032b74502cc494ee2d715763d
Alexandre Maurell <maurelllopes@mac.com>
2021-05-14 17:15:01 -0300

    Inserçao de usuario com POST

--- src/main/java/com/maurelllopes/workshopmongo/services/UserService.java
+++ src/main/java/com/maurelllopes/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.maurelllopes.workshopmongo.domain.User;
+import com.maurelllopes.workshopmongo.dto.UserDTO;
 import com.maurelllopes.workshopmongo.repository.UserRepository;
 import com.maurelllopes.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 		
 	}
 
