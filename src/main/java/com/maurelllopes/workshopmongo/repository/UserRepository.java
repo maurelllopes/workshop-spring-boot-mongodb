c9b8b6186c0423e10a9a2a788c55ad86dc05585f
Alexandre Maurell <maurelllopes@mac.com>
2021-05-14 01:27:19 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/maurelllopes/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.maurelllopes.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maurelllopes.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+}
---
+++ src/main/java/com/maurelllopes/workshopmongo/services/UserService.java
@@ -0,0 +1,23 @@
+package com.maurelllopes.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maurelllopes.workshopmongo.domain.User;
+import com.maurelllopes.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		
+		return repo.findAll();
+	}
+	
+
+}
